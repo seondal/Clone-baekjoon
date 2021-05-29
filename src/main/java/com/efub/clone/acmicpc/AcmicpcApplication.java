c316cc06b273a89a94f16222ff5ae6f7f6e92c52
hak2711 <hak271111@gmail.com>
2021-05-29 11:30:42 +0900

    init commit

---
+++ src/main/java/com/efub/clone/acmicpc/AcmicpcApplication.java
@@ -0,0 +1,13 @@
+package com.efub.clone.acmicpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmicpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmicpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efub/clone/acmicpc/AcmicpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.clone.acmicpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmicpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
