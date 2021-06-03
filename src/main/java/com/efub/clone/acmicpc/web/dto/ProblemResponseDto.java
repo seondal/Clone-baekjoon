8693a802a4e235db578aa5e65dce2b2476a06239
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-06-03 16:55:26 +0900

    UserDto 초안

--- src/main/java/com/efub/clone/acmicpc/web/dto/ProblemResponseDto.java
+++ src/main/java/com/efub/clone/acmicpc/web/dto/ProblemResponseDto.java
@@ -1,8 +1,22 @@
 package com.efub.clone.acmicpc.web.dto;
 
+import com.efub.clone.acmicpc.domain.problem.Problem;
+import com.efub.clone.acmicpc.domain.user.User;
 import lombok.Getter;
 
 @Getter
 public class ProblemResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String input_condition;
+    private String output_condition;
 
+    public ProblemResponseDto(Problem entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.input_condition = entity.getInput_condition();
+        this.output_condition = entity.getOutput_condition();
+    }
 }
