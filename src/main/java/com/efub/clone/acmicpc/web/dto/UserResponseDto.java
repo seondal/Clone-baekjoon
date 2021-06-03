9f0096d88a775cb166e3e5e0ca1dcbc55de1e315
hak2711 <hak271111@gmail.com>
2021-06-03 21:12:40 +0900

    Feat: "추가 User id로 조회 기능"
    UserController, UserService findById 추가

--- src/main/java/com/efub/clone/acmicpc/web/dto/UserResponseDto.java
+++ src/main/java/com/efub/clone/acmicpc/web/dto/UserResponseDto.java
@@ -4,12 +4,12 @@ import com.efub.clone.acmicpc.domain.user.User;
 import lombok.Getter;
 
 @Getter
-public class UerResponseDto {
+public class UserResponseDto {
     private String id;
     private String introduce;
     private Long rank;
 
-    public UerResponseDto(User entity){
+    public UserResponseDto(User entity){
         this.id = entity.getId();
         this.introduce = entity.getIntroduce();
         this.rank = entity.getRank();
