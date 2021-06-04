5ca8e319c4a05c9b20ac4e9f47d5e03e97b657b0
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-06-04 19:03:09 +0900

    새로운글 유저 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('testid',current_timestamp(),'hi hello',0,'testpw',5);
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('slygun',current_timestamp(), 'dk', 0, '1', 	105550);
+insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('omh9876',current_timestamp(), '오민혁2020114618', 0, '2', 6392);
+insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('fantasy7772',current_timestamp(), '김진욱', 0, '3', 11780);
+insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('nickse461',current_timestamp(), '안녕하세요', 0, '4', 56715);
+insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('pppp1923',current_timestamp(), '123456', 0, '5', 2621);
