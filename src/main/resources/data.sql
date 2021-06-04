70a162eee21bd5a1265638d52a600db475c81f7d
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-06-04 18:44:36 +0900

    유저1 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('testid',current_timestamp(),'hi hello',0,'testpw',5);
+insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('slygun',current_timestamp(), 'dk', 0, '1', 	105550);
