c3423b107e497a759ef8152a71965cc6cfbf8743
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-06-05 01:08:12 +0900

    problems 예제 추가 중

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,13 @@ insert into users(user_id, create_date, introduce, admin_flag, password, user_ra
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('nickse461',current_timestamp(), '안녕하세요', 0, '4', 56715);
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('pppp1923',current_timestamp(), '123456', 0, '5', 2621);
 
-insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition) values (21920, '서로소 평균', '효성이는 길이가 N인 수열 A에서 X와 서로소인 수들을 골라 평균을 구해보려고 한다. 효성이를 도와 이를 계산해주자.', '첫 번째 줄에 입력될 수들의 개수 N이 주어진다(N은 2이상 500,000이하). 두번째 줄에는 수열 A를 이루는 자연수 Ai가 공백과 함께 주어진다(Ai는 2이상 1,000,000이하). 수열 A에 X와 서로소인 수가 최소 1개 이상 존재한다. 마지막 줄에는 X가 주어진다(X는 2이상 1,000,000이하).' , '첫째 줄에 수열 A에서 X와 서로소인 수들의 평균을 출력한다. 절대/상대 오차는 1/1,000,000까지 허용한다.');
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition, multilingual_flag, problem_rank) values (21920, '서로소 평균', '효성이는 길이가 N인 수열 A에서 X와 서로소인 수들을 골라 평균을 구해보려고 한다. 효성이를 도와 이를 계산해주자.', '첫 번째 줄에 입력될 수들의 개수 N이 주어진다(N은 2이상 500,000이하). 두번째 줄에는 수열 A를 이루는 자연수 Ai가 공백과 함께 주어진다(Ai는 2이상 1,000,000이하). 수열 A에 X와 서로소인 수가 최소 1개 이상 존재한다. 마지막 줄에는 X가 주어진다(X는 2이상 1,000,000이하).' , '첫째 줄에 수열 A에서 X와 서로소인 수들의 평균을 출력한다. 절대/상대 오차는 1/1,000,000까지 허용한다.', false, 43234);
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition, multilingual_flag, problem_rank) values (21919, '소수 최소 공배수', '행복이는 길이가 N인 수열 A에서 소수들을 골라 최소공배수를 구해보려고 한다. 행복이르 도와 이를 계산해주자.', '첫째 줄에 수열 A의 길이 N이 주어진다(N은 1이상 10,000이하). 그 다음줄에는 수열 A의 원소 Ai가 공백과 함께 주어진다(Ai는 2이상 1,000,000이하). 답이 2^64 미만인 입력만 주어진다.', '첫째 줄에 소수들의 최소공배수를 출력한다. 만약 소수가 없는 경우는 -1을 출력한다.', false, 43233);
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition, multilingual_flag, problem_rank) values (21921, '블로그', '찬솔이는 블로그를 시작한 지 벌써 N일이 지났다. 요즘 바빠서 관리를 못 했다가 방문 기록을 봤더니 벌써 누적 방문 수가 6만을 넘었다. 찬솔이는 X일 동안 가장 많이 들어온 방문자 수와 그 기간들을 알고 싶다. 찬솔이를 대신해서 X일 동안 가장 많이 들어온 방문자 수와 기간이 몇 개 있는지 구해주자.', '첫째 줄에 블로그를 시작하고 지난 일수 N과 X가 공백과 함께 주어진다. 둘째 줄에는 블로그 시작 1일차부터 N일차까지 하루 방문자 수가 공백과 함께 주어진다.', '첫째 줄에 X일 동안 가장 많이 들어온 방문자 수를 출력한다. 만약 최대 방문자 수가 0명이라면 SAD를 출력한다. 만약 최대 방문자 수가 0명이 아닌 경우 둘째 줄에 기간이 몇 개 있는지 출력한다.', false, 43235);
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition, multilingual_flag, problem_rank) values
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition, multilingual_flag, problem_rank) values
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition, multilingual_flag, problem_rank) values
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition, multilingual_flag, problem_rank) values
 
-insert into status(user_id, problem_id, solved) values ('testid', 21920, 'solved');
+insert into board(board_id, board_title, problem_id, category, board_content, writer) values (69305, 'C언어 런타임에러 load of null 질문', 8958, '질문', '이 코드에서 5번째 줄을 char arr[80];으로 바꾸기만 하면 정답이 되던데 이 코드는 왜 안되는 걸까요?', 'slygun');
 
-insert into board(board_id, board_title, problem_id, category, writer, board_content) values (69305, 'C언어 런타임에러 load of null 질문', 8958, '질문', 'kimtawann', '이 코드에서 5번째 줄을 char arr[80];으로 바꾸기만 하면 정답이 되던데 이 코드는 왜 안되는 걸까요?');
