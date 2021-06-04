2a6c404bb0447cc4fbcde294d1519538cac21e3a
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-06-04 16:59:34 +0900

    HomeController getMapping 수정

--- src/main/java/com/efub/clone/acmicpc/web/HomeController.java
+++ src/main/java/com/efub/clone/acmicpc/web/HomeController.java
@@ -5,10 +5,9 @@ import com.efub.clone.acmicpc.service.ProblemService;
 import com.efub.clone.acmicpc.service.UserService;
 import com.efub.clone.acmicpc.web.dto.BoardListResponseDto;
 import com.efub.clone.acmicpc.web.dto.ProblemListResponseDto;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,15 +20,19 @@ public class HomeController {
     public final ProblemService problemService;
 
     @GetMapping
+    @RequestMapping(value = "/")
     public List<BoardListResponseDto> board_findAllDesc() { return boardService.findAllDesc(); }
 
     @GetMapping
+    @RequestMapping(value = "/")
     public List<ProblemListResponseDto> problem_findAllDesc() { return problemService.findAllDesc(); }
 
     @GetMapping
+    @RequestMapping(value = "/")
     public List<ProblemListResponseDto> findMultilingual() { return problemService.findMultilingualDesc(); }
 
     @GetMapping
+    @RequestMapping(value = "/")
     public List<ProblemListResponseDto> findAllSortByRank(){
         return problemService.findAllSortByRank();
     }
