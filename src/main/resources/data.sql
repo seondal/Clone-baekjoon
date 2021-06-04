7fd3cbc91fd872d5a005958ffe768458837b2942
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-06-04 23:48:04 +0900

    problems 예제 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,13 +5,7 @@ insert into users(user_id, create_date, introduce, admin_flag, password, user_ra
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('nickse461',current_timestamp(), '안녕하세요', 0, '4', 56715);
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('pppp1923',current_timestamp(), '123456', 0, '5', 2621);
 
-insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition) values (21920, '서로소 평균', '효성이는 길이가 N인 수열 A에서 X와 서로소인 수들을 골라 평균을 구해보려고 한다.
-
-효성이를 도와 이를 계산해주자.', '첫 번째 줄에 입력될 수들의 개수 N이 주어진다(N은 2이상 500,000이하). ' ||
-                    '두번째 줄에는 수열 A를 이루는 자연수 Ai가 공백과 함께 주어진다(Ai는 2이상 1,000,000이하).' ||
-                    '수열 A에 X와 서로소인 수가 최소 1개 이상 존재한다.' ||
-                    '마지막 줄에는 X가 주어진다(X는 2이상 1,000,000이하).' , '첫째 줄에 수열 A에서 X와 서로소인 수들의 평균을 출력한다.' ||
-                                                             '절대/상대 오차는 1/1,000,000까지 허용한다.');
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition) values (21920, '서로소 평균', '효성이는 길이가 N인 수열 A에서 X와 서로소인 수들을 골라 평균을 구해보려고 한다. 효성이를 도와 이를 계산해주자.', '첫 번째 줄에 입력될 수들의 개수 N이 주어진다(N은 2이상 500,000이하). 두번째 줄에는 수열 A를 이루는 자연수 Ai가 공백과 함께 주어진다(Ai는 2이상 1,000,000이하). 수열 A에 X와 서로소인 수가 최소 1개 이상 존재한다. 마지막 줄에는 X가 주어진다(X는 2이상 1,000,000이하).' , '첫째 줄에 수열 A에서 X와 서로소인 수들의 평균을 출력한다. 절대/상대 오차는 1/1,000,000까지 허용한다.');
 
 insert into status(user_id, problem_id, solved) values ('testid', 21920, 'solved');
 
