8e66e896bb535f9dd0b0d0341e084389c0205904
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-06-04 19:09:52 +0900

    table 틀 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,9 @@ insert into users(user_id, create_date, introduce, admin_flag, password, user_ra
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('fantasy7772',current_timestamp(), '김진욱', 0, '3', 11780);
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('nickse461',current_timestamp(), '안녕하세요', 0, '4', 56715);
 insert into users(user_id, create_date, introduce, admin_flag, password, user_rank) values('pppp1923',current_timestamp(), '123456', 0, '5', 2621);
+
+insert into problems(problem_id, problem_title, problem_content, input_condition, output_condition) values (d,d,d,d,d);
+
+insert into status() values ();
+
+insert into board() values ();
