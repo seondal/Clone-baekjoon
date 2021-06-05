d2b02d94eda65ac04e4f2abfd14949ccf5d5fb68
whkakrkr <whkakrkr@gmail.com>
2021-06-05 17:38:41 +0900

    프론트 hover 구현 및 수정

--- frontend/frontend/src/Container.js
+++ frontend/frontend/src/Container.js
@@ -1,27 +1,18 @@
 import React from "react";
 import axios from "axios";
-import {useState, useEffect} from "react";
-import {useHistory} from "react-router-dom";
-import active from "./assets/active.png";
-import notactive from "./assets/notactive.png";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import active from "./assets/blue.png";
+import notactive from "./assets/gray.png";
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    font-size: 13px;
-`
-
-const Context = styled.div `
-    display: flex;
-    flex-direction: column;
-    padding:15px;
-`
-
-const Title = styled.div`
-    border-bottom: 1px;
-    border-bottom-color: var(--point);
+padding-top:40px;
+padding-bottom: 40px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+font-size: 13px;    
 
 a:link{
     color : var(--content);
@@ -34,79 +25,187 @@ a:visited{
 a:hover  {
     text-decoration: underline;
 }
-    
+`
+
+const Context = styled.div`
+display: flex;
+flex-direction: column;
+padding:15px;
+`
+
+const Title = styled.div`
+margin-top:10px;
+margin-bottom: 25px;
+h2{
+    margin: 0px;
+    width: fit-content;
+    padding: 5px;
+    border-bottom: 2px solid;
+    border-color: var(--point);
+}    
 `
 
 const Card = styled.div`
-img{
-    width:255px;
-    height:40px;
-    margin-bottom:10px;
+width:255px;
+height: auto;
+padding-top: 10px;
+padding-left: 10px;
+margin-bottom: 10px;
+background-color : #F7F7F7;
+flex-wrap: wrap;
+
+a {
+color: #333333;
+text-decoration: none;
+    :hover {
+    text-decoration: underline;
+    }
+}
+.triangle{
+width:15px;
+height:15px;
+background : url(${notactive});
+background-size : 100%;
+margin: 0px;
+display: inline-block;
+margin-left: 240px;
 }
 
-
+:hover{
+    .triangle {
+    width:15px;
+    height:15px;
+    background : url(${active});
+    background-size : 100%;
+    }
+    
+} 
 `
-
 
 function Container() {
     //문제 순위
     const [rankdata, setRankdata] = useState();
     useEffect(async () => {
-        try{
-            const response=await axios.get(`http://localhost:8080/problem/sort/rank`);
+        try {
+            const response = await axios.get(`http://localhost:8080/problem/sort/rank`);
             setRankdata(response.data); //데이터 저장
-        } catch(e) {
+        } catch (e) {
             console.log("error");
         }
     }, []);
 
     return (
-        
+
         <Wrapper>
             <Context>
-                <Title><h2><a href=''>새로운 글</a></h2></Title>
-                <Card><img src={notactive} style={{height:"77px"}}/></Card>
-                <Card><img src={notactive} style={{height:"77px"}}/></Card>
-                <Card><img src={notactive} style={{height:"77px"}}/></Card>
-                <Card><img src={notactive} style={{height:"77px"}}/></Card>
-
+            <Title><h2><a href="">새로운 글</a></h2></Title>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
             </Context>
 
             <Context>
-                <Title><h2><a href=''>추가된 문제</a></h2></Title>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
+            <Title><h2><a href="">새로운 글</a></h2></Title>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
             </Context>
 
             <Context>
-                <Title><h2><a href=''>추가된 영어문제</a></h2></Title>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
-                <Card><img src={notactive}/></Card>
+            <Title><h2><a href="">새로운 글</a></h2></Title>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
+                <Card>
+                    <a href="">21925번 짝수 팰린드롬</a>
+                    <div class="triangle"></div>
+                </Card>
             </Context>
-
+        
             <Context>
                 <Title><h2><a href=''>문제 순위</a></h2></Title>
                 {rankdata?.map((item, i) => {
-                    if (i<7) 
+                    if (i < 7)
                         return (
                             <Card>
-                                <a href="" style={{position:'absolute', color:'#333', fontSize:'15px', margin:'10px', textDecoration:'none'}}>
+                                <a href="" style={{ position: 'absolute', color: '#333', fontSize: '15px', margin: '10px', textDecoration: 'none' }}>
                                     {item.id}번 {item.title}
                                 </a>
-                                <img src={notactive}/>
+                                <img src={notactive} />
                             </Card>
                         )
-                    }
+                }
                 )}
             </Context>
 
@@ -115,7 +214,7 @@ function Container() {
             </Context>
         </Wrapper>
     );
-    
+
 }
 
 export default Container;
